661ca4ac2e6c0c7b88b0eff42ad7390c91711d41
fsw <frank@wawrzak.com>
2018-01-22 20:45:07 +0100

    fix

--- mkcmt.sh
+++ mkcmt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-DAYS="022 023 024 025 026"
+DAYS="022 023 024 025 026 029 031 036"
 MATCH=$(date +%j)
 
 if echo $DAYS | grep -qw $MATCH; then
 	echo "YES!"
-	for run in {1..10}
+	for run in {1..25}
 	do
 		echo "RUN $run"
 		date +%s | sha256sum | base64 | head -c 32 > ./content.txt
