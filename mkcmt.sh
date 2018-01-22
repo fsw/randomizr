26a4319078971b4bc5c58d621009cdefc41557da
fsw <frank@wawrzak.com>
2018-01-22 20:35:04 +0100

    random change

--- mkcmt.sh
+++ mkcmt.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-date +%s | sha256sum | base64 | head -c 32 > ./content.txt
-
-git commit -a -m 'random change'
-
-git push
-
+for run in {1..10}
+do
+	date +%s | sha256sum | base64 | head -c 32 > ./content.txt
+	git commit -a -m 'random change'
+	git push
+done
 
