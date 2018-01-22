a0fb6a0da6e574ee9ab531b28616c0199eb128fc
fsw <frank@wawrzak.com>
2018-01-22 20:42:06 +0100

    random change

--- mkcmt.sh
+++ mkcmt.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-for run in {1..10}
-do
-	echo "RUN $run"
-	date +%s | sha256sum | base64 | head -c 32 > ./content.txt
-	git commit -a -m 'random change'
-	git push
-done
+
+DAYS="022 023 024 025 026"
+MATCH=$(date +%j)
+
+if echo $DAYS | grep -qw $MATCH; then
+	echo "YES!"
+	for run in {1..10}
+	do
+		echo "RUN $run"
+		date +%s | sha256sum | base64 | head -c 32 > ./content.txt
+		git commit -a -m 'random change'
+		git push
+	done
+else
+	echo "NOPE"
+	exit 1
+fi
+
 
