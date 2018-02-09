4dfee3220bd94f79ca90721cb7f437518c382ecc
fsw <frank@wawrzak.com>
2018-02-09 12:42:29 +0100

    random change

--- mkcmt.sh
+++ mkcmt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-DAYS="022 023 024 025 026 029 031 036 037 038 039 40"
+DAYS="022 023 024 025 026 029 031 036 037 038 039 040"
 MATCH=$(date +%j)
 
 if echo $DAYS | grep -qw $MATCH; then
